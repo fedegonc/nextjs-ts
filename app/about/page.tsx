8f6226f980ed4ee72b4b4f306abadcae9a0f66ed
federico goncalvez <97002761+fedegonc@users.noreply.github.com>
2024-11-23 11:21:56 -0300

    Create page.tsx

---
+++ app/about/page.tsx
@@ -0,0 +1,8 @@
+export default function About() {
+  return (
+    <div className="p-8">
+      <h1 className="text-2xl font-bold">Sobre Nosotros</h1>
+      <p>Conoce más sobre nuestra empresa y misión.</p>
+    </div>
+  );
+}
